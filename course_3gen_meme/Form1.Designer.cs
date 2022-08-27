9aa18712bf19054730bfe4d590d44fd67e1d2e8f
denismaksyutenko <d.maksyutenko@student.csn.khai.edu>
2022-08-27 12:47:05 +0300

    gen+meme_course3
    gen_meme v1.0
    first program for my project

---
+++ course_3gen_meme/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace course_3gen_meme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(685, 503);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(792, 451);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(691, 451);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.trackBar1);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(691, 43);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(191, 84);
+            this.flowLayoutPanel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "size pen";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(3, 41);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(173, 56);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(688, 146);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 72);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button14
+            // 
+            this.button14.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button14.Location = new System.Drawing.Point(786, 260);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(90, 30);
+            this.button14.TabIndex = 12;
+            this.button14.Text = "clear text";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button15.Location = new System.Drawing.Point(691, 296);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(185, 40);
+            this.button15.TabIndex = 13;
+            this.button15.Text = "input image";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(699, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 25);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 14;
+            this.label4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label4_MouseClick);
+            this.label4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label4_MouseDown);
+            this.label4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label4_MouseMove);
+            this.label4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label4_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(688, 224);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 30);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "font";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(787, 224);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 30);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "back color";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 503);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.MinimumSize = new System.Drawing.Size(800, 550);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+    }
+}
+
---
+++ course_3gen_meme/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace course_3gen_meme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Setsize();//инициализация bitmap и graphics
+            var pos = this.PointToScreen(label4.Location);//переменная места метки
+            pos = pictureBox1.PointToClient(pos);//присвоение пикчебоксу
+            label4.Parent = pictureBox1;
+            label4.Location = pos;// на позицию пикчебокса
+            label4.BackColor = Color.Transparent;//прозрачность заднего фона надписи
+        }
+        private class Arraypoints
+        {
+            private int index = 0;//номер текущей точки в массиве
+            private Point[] points;//массив точек
+
+            public Arraypoints(int size)//конструктор для задания размера
+            {
+                if (size <= 0) size = 2;// size - размер массива
+                points = new Point[size];//инициализация массива
+            }
+            public void Setpoints (int x, int y)//принимает координаты точки
+            {
+                if (index >= points.Length)//проверка номера точки, не выходит ли за границы
+                    index = 0;
+                points[index] = new Point(x, y);//присвоение новой точки
+                index++;
+            }
+            public void resetpoints()//сброс точек, при отпускании лкм
+            {
+                index = 0;
+            }
+            public int getcountpoints()//возврат текущего индекса, сколько точек в массиве
+            {
+                return index;
+            }
+            public Point[] getpoints()//возврат массива точек, для отрисовки
+            {
+                return points;
+            }
+        }
+
+        private bool isMouse = false;//проверка на зажатую лкм
+        private Arraypoints arrayPoints = new Arraypoints(2); //экземпляр массива из двух точек
+        Bitmap bitmap = new Bitmap(100, 100);//отвечает за хранение изображения
+        Graphics graphics;
+        Pen pen = new Pen(Color.Black, 3f);//экземпляр кисти
+        private object currentlabel = null;// выбор метки
+
+        // задача размера для bitmap
+        private void Setsize()
+        {
+            Rectangle rectangle = Screen.PrimaryScreen.Bounds;//определение разрешения пользователя
+            bitmap = new Bitmap(rectangle.Width, rectangle.Height);//передача в bitmap
+            graphics = Graphics.FromImage(bitmap);// передача переменной bitmap
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;//сглаживание линии
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;//сглаживание линии
+        }
+        //при нажатии мыши переводит isMouse в состояние true
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouse = true;
+        }
+        //при отжиме кнопки мыши переводит isMouse в состояние false и сбрасывает массив точек
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouse = false;
+            arrayPoints.resetpoints();
+        }
+        // выбор цвета кисти из палитры цветов
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pen.Color = colorDialog1.Color;
+            ((Button)sender).BackColor = colorDialog1.Color;
+        }
+        //кнопка очистки
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            label4.BackColor = Color.White;
+            label4.Text = null;
+            graphics.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = bitmap;
+        }
+        // отвечает за толшину линии кисти
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            pen.Width = trackBar1.Value;
+        }
+        // сохранение рисунка
+        private void button1_Click(object sender, EventArgs e)
+        {
+            graphics.DrawString(label4.Text, new Font("Arial", 14), Brushes.Black, label4.Location.X, label4.Location.Y);
+            label4.Text = null;
+            saveFileDialog1.Filter = "JPG(*.jpg)|*.jpg|PNG(*.png)|*.png|Все файлы(*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                if (pictureBox1.Image != null)
+                    pictureBox1.Image.Save(saveFileDialog1.FileName);
+        }
+        
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isMouse) { return; } //если мышка не нажата, ничего не делать
+            arrayPoints.Setpoints(e.X, e.Y); //установка начала линии, заполнение массива
+            if (arrayPoints.getcountpoints() >= 2)//проверка на к-во точек
+            {
+                graphics.DrawLines(pen, arrayPoints.getpoints());// рисует
+                pictureBox1.Image = bitmap;//присвоение рисунка
+                arrayPoints.Setpoints(e.X, e.Y);//чтоб линия не прирывалась
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (currentlabel != null) //проверка выбраной метки
+            {   // oчистка
+                textBox1.Clear();
+                label4.BackColor = Color.White;
+                label4.Text = null;
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog name = new OpenFileDialog();
+            name.Filter = "JPG(*.jpg)|*.jpg|PNG(*.png)|*.png|Все файлы(*.*)|*.*";
+            if(name.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(name.FileName);
+                    graphics.DrawImage(pictureBox1.Image, pictureBox1.Location.X, pictureBox1.Location.Y);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+                catch
+                {
+                    MessageBox.Show("can not open file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void label4_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouse = true;
+        }
+
+        private void label4_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouse = false;
+        }
+
+        private void label4_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isMouse) { return; } //если мышка не нажата, ничего не делать
+            if (currentlabel != null) //проверка выбраной метки
+                currentlabel.GetType().GetProperty("Location").SetValue(currentlabel, new Point(Cursor.Position.X - 100, Cursor.Position.Y - 100));// перемещение выбраной метки
+        }
+
+        private void label4_MouseClick(object sender, MouseEventArgs e)//для работы с выбраной меткой
+        {
+            currentlabel = sender;
+        }
+        // выбор шрифта, размера и цвета
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() != DialogResult.OK) { return; }// задание шрифта
+            label4.Font = fontDialog1.Font;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (colorDialog2.ShowDialog() == DialogResult.OK)
+                label4.BackColor = colorDialog2.Color; //задание цвета
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label4.Text = textBox1.Text; //отображение текста на метке
+            pictureBox1.Image = bitmap;// отображение текста
+        }
+    }
+}
